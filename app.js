ddf2c34dfd1300f6c619171f49351d5e8e1dfeac
achrefrouabeh <achref.rouabeh@gmail.com>
2018-12-21 19:14:20 +0100

    state variabels

--- app.js
+++ app.js
@@ -9,30 +9,35 @@ GAME RULES:
 
 /* Game Init Variables */
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
 document.querySelector(".btn-roll").addEventListener("click", () => {
-  // 1. RANDOM NUMBER
-  let dice = Math.floor(Math.random() * 6) + 1;
-  //2. DISPLAY THE RESULT
-  let diceDom = document.querySelector(".dice");
-  diceDom.style.display = "block";
-  diceDom.src = "dice-" + dice + ".png";
-  //3. UPDATE THE ROUND SCORE IF NOT == 1
-  if (dice !== 1) {
-    //ADD SCORE
-    roundScore += dice;
-    document.querySelector("#current-" + activePlayer).textContent = roundScore;
-  } else {
-    //NEXT PLAYER
-    nextPlayer();
+  if (gamePlaying) {
+    // 1. RANDOM NUMBER
+    let dice = Math.floor(Math.random() * 6) + 1;
+    //2. DISPLAY THE RESULT
+    let diceDom = document.querySelector(".dice");
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + dice + ".png";
+    //3. UPDATE THE ROUND SCORE IF NOT == 1
+    if (dice !== 1) {
+      //ADD SCORE
+      roundScore += dice;
+      document.querySelector(
+        "#current-" + activePlayer
+      ).textContent = roundScore;
+    } else {
+      //NEXT PLAYER
+      nextPlayer();
+    }
   }
 });
 
 document.querySelector(".btn-hold").addEventListener("click", () => {
-  //ADD CURRENT SCORE TO GLOBAL SCORE
+  if (gamePlaying) {
+    //ADD CURRENT SCORE TO GLOBAL SCORE
   scores[activePlayer] += roundScore;
   //UPDATE THE UI
   document.querySelector("#score-" + activePlayer).textContent =
@@ -47,10 +52,13 @@ document.querySelector(".btn-hold").addEventListener("click", () => {
     document
       .querySelector(".player-" + activePlayer + "-panel")
       .classList.remove("active");
+      gamePlaying = false;
   } else {
     //NEXT PLAYER
     nextPlayer();
   }
+  }
+  
 });
 
 function nextPlayer() {
@@ -74,6 +82,7 @@ function init() {
   scores = [0, 0];
   activePlayer = 0;
   roundScore = 0;
+  gamePlaying = true;
 
   /* SELECT THE DOM */
   document.querySelector(".dice").style.display = "none";
@@ -89,5 +98,4 @@ function init() {
   document.querySelector(".player-0-panel").classList.remove("active");
   document.querySelector(".player-1-panel").classList.remove("active");
   document.querySelector(".player-0-panel").classList.add("active");
-  
 }
