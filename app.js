4ee019e92ed5aa510f2aaaba313de768e68206e9
achrefrouabeh <achref.rouabeh@gmail.com>
2018-12-21 15:49:35 +0100

    change the html class

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ GAME RULES:
 let scores, roundScore, activePlayer;
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 /* SELECT THE DOM */
 document.querySelector(".dice").style.display = "none";
@@ -30,4 +30,18 @@ document.querySelector(".btn-roll").addEventListener("click", () => {
   diceDom.style.display = "block";
   diceDom.src = "dice-" + dice + ".png";
   //3. UPDATE THE ROUND SCORE IF NOT == 1
+  if (dice !== 1) {
+    //ADD SCORE
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    //NEXT PLAYER
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    document.querySelector(".player-0-panel").classList.remove("active");
+    document.querySelector(".player-1-panel").classList.add("active");
+  }
 });
